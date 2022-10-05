8b2f9fd55332fce18e1ef8e900ac9eb2f6946553
Tag143 <jtw93843@uga.edu>
2022-10-05 19:52:48 -0400

    Data_mem_tb.v v2

--- src/Data_mem_tb.v
+++ src/Data_mem_tb.v
@@ -12,6 +12,7 @@ module Data_mem_tb();
     end
 
     integer inc;
+    parameter OFFSET = 1024;
     // Testing
     initial begin
     $dumpvars(0, Data_mem_tb);
@@ -19,13 +20,47 @@ module Data_mem_tb();
         @(posedge clk_s);
         @(posedge clk_s);
         @(posedge clk_s);
-        // Write enable on
+        // CASE: Write to one memory location
+        @(posedge clk_s);
+        #5 we <= 0;
+        #5 Data_address_s <= OFFSET;
+        #5 Data_in_s <= 32'hFFFFFFFF;
+        @(posedge clk_s);
+        #5 we <= 0;
+        @(posedge clk_s);
+        // CASE: Should not write anything
+        @(posedge clk_s);
+        #5 Data_address_s <= OFFSET + 500; // change chosen address for good measure
+        #5 Data_in_s <= 32'hFFFFFFFF;
+        @(posedge clk_s);
+        // CASE: Rewrite to that memory
+        @(posedge clk_s);
         #5 we <= 1;
-        // Reg data_memory should (maybe) already have contents, pass clocks and they will be loaded into memory
-        // For loop to pass many clock edges
-        for(inc = 0; inc < 300; inc = inc + 1) begin
-            @(posedge clk_s); // pass 300 clocks
+        #5 Data_address_s <= OFFSET;
+        #5 Data_in_s <= 32'h0000AAAA;
+        @(posedge clk_s);
+        #5 we <= 0;
+        @(posedge clk_s);
+        @(posedge clk_s);
+        // CASE: Write to a block of memory in loop
+        @(posedge clk_s);
+        #5 we <= 1;
+        #5 Data_in_s <= 32'hFFFFFFFF;
+        // For loop to increment address and write to memory
+        for (inc = 0; inc < 50; inc = inc + 1) begin
+            # 5 Data_address_s <= OFFSET + inc;
+            @(posedge clk_s);
         end
+        #5 we <= 0; // end write to block
+        @(posedge clk_s);
+        @(posedge clk_s);
+        @(posedge clk_s);
         $finish;
     end
 endmodule
+
+  // // Reg data_memory should (maybe) already have contents, pass clocks and they will be loaded into memory
+        // // For loop to pass many clock edges
+        // for(inc = 0; inc < 300; inc = inc + 1) begin
+        //     @(posedge clk_s); // pass 300 clocks
+        // end
