1c46f9be7e50700cec26f1d8f1e07447bb2f5876
Omer Cinar <omercinar253@gmail.com>
2022-10-05 05:55:44 -0400

    Updated data_mem to a hopefully working status. Potential change with sync/async read but it compiles

--- src/Data_mem.v
+++ src/Data_mem.v
@@ -2,19 +2,25 @@ module Data_mem(
     input clk,
     input [31:0] data_address,
     input [31:0] data_in,
-    input write_e,
+    input we,
     output reg [31:0] data_out
 );
 
 
-// Data memory block (1024*4) bytes of space
-reg [31:0] data_memory [0:1024];
+// Data memory block (1024*4) bytes of space or just 1024 DWORDS lmao 
+reg [31:0] data_memory [0:1023];
 
 // Load data_memory with contents of file (assuming file is in hex)
 initial begin
     $readmemh("FILENAME.???", data_memory);
 end
 
-always
+// read/write synchronous block
+always @(posedge clk) begin
+    if(we) 
+        data_memory[data_address] <= data_in;
+    data_out <= data_memory[data_address];
+end
+
 
 endmodule
