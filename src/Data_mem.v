e38967e9caac60f2895b526ef54ee0cc03db6c9e
Omer Cinar <omercinar253@gmail.com>
2022-10-05 05:14:18 -0400

    Updated in_mem with comments and mapped io of data_mem

--- src/Data_mem.v
+++ src/Data_mem.v
@@ -1,6 +1,20 @@
 module Data_mem(
     input clk,
-    input 
+    input [31:0] data_address,
+    input [31:0] data_in,
+    input write_e,
+    output reg [31:0] data_out
 );
+
+
+// Data memory block (1024*4) bytes of space
+reg [31:0] data_memory [0:1024];
+
+// Load data_memory with contents of file (assuming file is in hex)
+initial begin
+    $readmemh("FILENAME.???", data_memory);
+end
+
+always
 
 endmodule
--- src/In_mem.v
+++ src/In_mem.v
@@ -1,16 +1,19 @@
 module In_mem(
     input clk,
     input [31:0] address,
-    output reg [31:0] data_a
+    output reg [31:0] instr_out
 );     
-    reg [31:0] memory[0:255];
+    reg [31:0] in_memory[0:255];
 
+    // Load in_memory block with values of mem file
     initial begin
-        $readmemh("FILENAME.mem", memory);
+        $readmemh("FILENAME.mem", in_memory);
     end
 
+
+    // Fetch instruction at in address and put it into instr_out
     always @(posedge clk) begin
-        data_a <= memory[address];
+        instr_out <= in_memory[address];
     end
 
 endmodule
